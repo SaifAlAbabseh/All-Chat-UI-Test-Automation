057226f77c48ebefa5607599601abb972f82ec3a
SAIF_ALABABSEH\sabab <sababseh97@gmail.com>
2025-03-07 00:41:36 +0300

    ...

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -41,6 +41,7 @@ public class MainTest extends TestBase {
         }
         assertTrue(mainPage.getAddNewFriendLink().isDisplayed());
         assertTrue(mainPage.getEditProfileLink().isDisplayed());
-        assertTrue(mainPage.getUsernameLabel().isDisplayed());
+        assertTrue(false);
+        //mainPage.getUsernameLabel().isDisplayed()
     }
 }
