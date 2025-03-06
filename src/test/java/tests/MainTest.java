d1265888b0306c0faaa9d8062bc168fe77d758c1
SAIF-ALABABSEH\sabab <sababaseh97@gmail.com>
2025-03-06 09:06:51 +0300

    ...

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -41,6 +41,7 @@ public class MainTest extends TestBase {
         }
         assertTrue(mainPage.getAddNewFriendLink().isDisplayed());
         assertTrue(mainPage.getEditProfileLink().isDisplayed());
-        assertTrue(mainPage.getUsernameLabel().isDisplayed());
+        // mainPage.getUsernameLabel().isDisplayed()
+        assertTrue(false);
     }
 }
