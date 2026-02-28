de2c01b7cf5dbafa94de2ec3f4d700f5d654e0fe
SaifAlAbabseh <sababseh97@gmail.com>
2026-02-28 04:18:54 +0300

    test

--- src/main/java/utils/EnvConfig.java
+++ src/main/java/utils/EnvConfig.java
@@ -4,7 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().systemProperties().load();
 
     public static String get(String key) {
         return dotenv.get(key);
