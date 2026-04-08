31f2b556dff95783b737a6e876013b3661245e55
SaifAlAbabseh <sababseh97@gmail.com>
2026-04-09 01:27:10 +0300

    test

--- src/test/java/runners/base/BaseRunnerTest.java
+++ src/test/java/runners/base/BaseRunnerTest.java
@@ -23,28 +23,28 @@ public class BaseRunnerTest extends AbstractTestNGCucumberTests {
         DriverManager.setUp(mobileMode, browser, headlessMode);
     }
 
-    @AfterTest
-    public void handleCucumberReports() {
-        Object[] driverConfig = DriverManager.driverConfig.get();
-
-        boolean mobileMode = (Boolean)driverConfig[0];
-        String browserName = (String)driverConfig[1];
-        boolean headlessMode = (Boolean)driverConfig[2];
-
-        String fileName = String.format("cucumber-report-%s-%s-%s", mobileMode ? "Mobile View" : "Desktop View", browserName.toUpperCase(), headlessMode ? "Headless" : "Headed");
-
-        //Handle HTML report
-        File htmlReport = new File("target/cucumber-report.html");
-        File newHtmlReport = new File(String.format("target/%s.html", fileName));
-        if(htmlReport.exists()) {
-            htmlReport.renameTo(newHtmlReport);
-        }
-
-        //Handle JSON report
-        File jsonReport = new File("target/cucumber-report.json");
-        File newJsonReport = new File(String.format("target/%s.json", fileName));
-        if(jsonReport.exists()) {
-            jsonReport.renameTo(newJsonReport);
-        }
-    }
+//    @AfterTest
+//    public void handleCucumberReports() {
+//        Object[] driverConfig = DriverManager.driverConfig.get();
+//
+//        boolean mobileMode = (Boolean)driverConfig[0];
+//        String browserName = (String)driverConfig[1];
+//        boolean headlessMode = (Boolean)driverConfig[2];
+//
+//        String fileName = String.format("cucumber-report-%s-%s-%s", mobileMode ? "Mobile View" : "Desktop View", browserName.toUpperCase(), headlessMode ? "Headless" : "Headed");
+//
+//        //Handle HTML report
+//        File htmlReport = new File("target/cucumber-report.html");
+//        File newHtmlReport = new File(String.format("target/%s.html", fileName));
+//        if(htmlReport.exists()) {
+//            htmlReport.renameTo(newHtmlReport);
+//        }
+//
+//        //Handle JSON report
+//        File jsonReport = new File("target/cucumber-report.json");
+//        File newJsonReport = new File(String.format("target/%s.json", fileName));
+//        if(jsonReport.exists()) {
+//            jsonReport.renameTo(newJsonReport);
+//        }
+//    }
 }
